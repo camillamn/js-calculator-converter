a7fc7c5da73d9503ad4af48bc94e2dd8d369cc47
Camilla <112381378+camillamn@users.noreply.github.com>
2022-11-03 10:31:11 +0100

    Added styling and flex

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -1,14 +1,18 @@
 <template>
 		<main>
-		<h1>Kalkulator og konverterer</h1>
+		<h1>Kalkulator</h1>
 		<div class="calculator">
 			<div class="display">
 				123
 			</div>
 			<div class="buttons">
 				<div class="button-row" v-for="row in buttonRows">
-					<div class="button" v-for="button in row"></div>
-
+					<div
+						:class="{operator: button.type == 'operator'}"
+						class="button" 
+						v-for="button in row">
+						{{ button.text }}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -21,17 +25,17 @@
 			buttonRows: [
 				[{ 
 					text: 'C',
-					type: 'operator'
+					type: 'special'
 				}, {
 					text: '()',
-					type: 'operator'
+					type: 'special'
 				}, { 
 					text: '%',
-					type: 'operator'
+					type: 'special'
 				}, {
 					text: '/',
 					type: 'operator'
-				}]
+				}],
 				[{ 
 					text: '1',
 					type: 'number'
@@ -44,7 +48,7 @@
 				}, {
 					text: '*',
 					type: 'operator'
-				}]
+				}],
 				[{ 
 					text: '4',
 					type: 'number'
@@ -57,7 +61,7 @@
 				}, {
 					text: '-',
 					type: 'operator'
-				}]
+				}],
 				[{ 
 					text: '7',
 					type: 'number'
@@ -70,7 +74,7 @@
 				}, {
 					text: '+',
 					type: 'operator'
-				}]
+				}],
 				[{ 
 					text: '0',
 					type: 'number'
@@ -86,3 +90,52 @@
 	}
 </script>
 
+<style>
+body {
+	width: 100vw;
+	height: 100vh;
+}
+.calculator {
+	width: 30vw;
+	font-family: Georgia, 'Times New Roman', Times, serif;
+	border-radius: 20px;
+	overflow: hidden;
+}
+
+.display {
+	background-color: darkgray;
+	color: white;
+	text-align: right;
+	padding: 1em;
+	border-radius: 5%;
+}
+.buttons {
+text-align: center;
+}
+
+.button-row {
+	display: flex;
+	justify-content: space-around;
+}
+
+.button {
+	display: inline-block;
+	border: 1px solid gray;
+	border-radius: 25%;
+	margin: 0.2em;
+	width: 100%;
+	padding: 0.5em;
+	background-color: aquamarine;
+}
+
+.operator {
+	background-color: cadetblue;
+	color: white;
+}
+
+.button:active,
+.button:hover {
+	background-color: rgb(40, 145, 110);
+}
+
+</style>
