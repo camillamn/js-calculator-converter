4ac744784c2629edbd3bfb65468ac52df63b23f8
Camilla <112381378+camillamn@users.noreply.github.com>
2022-11-03 08:52:35 +0100

    Set up project-structure

---
+++ app/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<main>
+		<h1>Kalkulator</h1>
+		<div class="calculator">
+			<div class="display"></div>
+		</div>
+	</main>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+	.calculator {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-auto-rows: minmax(50px, auto);
+	}
+
+	.display {
+		grid-column: 1 / 5;
+		background-color: aquamarine;
+	}
+</style>
